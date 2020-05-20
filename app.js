ebdd0b994ec524d832fa6ec6471a8cee23a8b9d4
Graham Dodsworth <graham.dodsworth@nhs.net>
2020-05-20 21:50:01 +0100

    Start navigating nodes to get attribute values and display them

--- app.js
+++ app.js
@@ -10,8 +10,20 @@ fs.readFile('./vacc_example.xml', (err, data) => {
     return
   }
   parser.parseString(data, function (err, result) {
-    	console.dir(result);
-    	console.log(util.inspect(result, false, null));
-        console.log('Done');
+    	//console.dir(result);
+    	//console.log(util.inspect(result, false, null));
+      var type               = result.Bundle.type[0].$.value;
+      var identifierSystem   = result.Bundle.entry[1].resource[0].Immunization[0].identifier[0].system[0].$.value;
+      var identifierValue    = result.Bundle.entry[1].resource[0].Immunization[0].identifier[0].value[0].$.value;
+      var vaccineCodeSNOMED  = result.Bundle.entry[1].resource[0].Immunization[0].vaccineCode[0].coding[0].code[0].$.value;
+      var vaccineCodeDisplay = result.Bundle.entry[1].resource[0].Immunization[0].vaccineCode[0].coding[0].display[0].$.value;
+      var notGiven           = result.Bundle.entry[1].resource[0].Immunization[0].notGiven[0].$.value;
+      console.log( "type = " + type);
+      console.log( "identifier (system) = " + identifierSystem);
+      console.log( "identifier (value) = " + identifierValue);
+      console.log( "vaccineCode (value) = " + vaccineCodeSNOMED);
+      console.log( "vaccineCode (display) = " + vaccineCodeDisplay);
+      console.log( "notGiven = " + notGiven);
+      console.log('Done');
   });
 })
