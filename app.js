580d64b98b8d43e09663e12df082cfda978b67ee
Graham Dodsworth <graham.dodsworth@nhs.net>
2020-06-15 21:00:01 +0100

    add parsing and rendering of NRL multi vacc

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var fs      = require('fs'),
 var parser = new xml2js.Parser();
 var app = express();
 var port = 3000;
-var fileToParse = "vacc_example_nems.xml";
+var NEMSfileToParse = "vacc_example_nems.xml";
+//var NRLfileToParse = "vacc_example_nrl_device.xml";
+var NRLfileToParse = "vacc_example_nrl_device3.xml";
 
 global.fileToValidate = "test.xml";
 
@@ -19,11 +21,12 @@ app.get("/",function(req,res){
   res.render("home");
 });
 
-app.get("/single",function(req,res){
 
-  console.log("Attempting to parse: " + fileToParse);
+app.get("/multi",function(req,res){
 
-  fs.readFile('./' + fileToParse, (err, data) => {
+  console.log("Attempting to parse: " + NRLfileToParse);
+
+  fs.readFile('./' + NRLfileToParse, (err, data) => {
     if (err) {
       console.error(err)
       return
@@ -34,8 +37,100 @@ app.get("/single",function(req,res){
         //console.log(util.inspect(result, false, null));
 
         var numberOfResources = result.Bundle.entry.length - 1;
-        console.log("numberOfResources : " + numberOfResources);
-        console.log("type : " + typeof numberOfResources);
+        console.log("NRL numberOfResources : " + numberOfResources);
+        console.log("NRL type : " + typeof numberOfResources);
+
+        global.identifierSystem = [];
+        global.identifierValue = [];
+        global.vaccineProcedureCode = [];
+        global.vaccineProcedureDisplay = [];
+        global.notGiven = [];
+        global.vaccineCodeSNOMED = [];
+        global.vaccineCodeDisplay = [];
+        global.dateChar = [];
+
+        global.vaccCount = 0;
+
+        for (i = 1; i <= numberOfResources; i++) {
+          var tempResource = JSON.stringify( result.Bundle.entry[i].resource[0] );
+          var endBit = tempResource.indexOf('":');
+          var tempResource2 = tempResource.slice(2,endBit);
+          console.log("Resource : " + i + " " + tempResource2);
+
+          if (tempResource2 == "Immunization")  {
+        //global.vaccCount == global.vaccCount+1;
+        global.vaccCount++;
+           // Immunization resource
+            global.identifierSystem[i]  = result.Bundle.entry[i].resource[0].Immunization[0].identifier[0].system[0].$.value;
+            global.identifierValue[i]    = result.Bundle.entry[i].resource[0].Immunization[0].identifier[0].value[0].$.value;
+            global.vaccineProcedureCode[i] = result.Bundle.entry[i].resource[0].Immunization[0].extension[0].valueCodeableConcept[0].coding[0].code[0].$.value;
+            global.vaccineProcedureDisplay[i] = result.Bundle.entry[i].resource[0].Immunization[0].extension[0].valueCodeableConcept[0].coding[0].display[0].$.value;     
+            global.notGiven[i]           = result.Bundle.entry[i].resource[0].Immunization[0].notGiven[0].$.value;
+            global.vaccineCodeSNOMED[i]  = result.Bundle.entry[i].resource[0].Immunization[0].vaccineCode[0].coding[0].code[0].$.value;
+            global.vaccineCodeDisplay[i] = result.Bundle.entry[i].resource[0].Immunization[0].vaccineCode[0].coding[0].display[0].$.value;
+            global.dateChar[i]           = result.Bundle.entry[i].resource[0].Immunization[0].date[0].$.value;
+          }
+
+          if (tempResource2 == "HealthcareService")  {
+            // HealthcareService resource
+            global.hcsSNOMED           = result.Bundle.entry[i].resource[0].HealthcareService[0].type[0].coding[0].code[0].$.value;
+            global.hcsDisplay          = result.Bundle.entry[i].resource[0].HealthcareService[0].type[0].coding[0].display[0].$.value;
+          }
+
+          if (tempResource2 == "Patient")  {
+            // Patient resource
+            global.nhsnumber          = result.Bundle.entry[i].resource[0].Patient[0].identifier[0].value[0].$.value;
+            global.patGiven           = result.Bundle.entry[i].resource[0].Patient[0].name[0].given[0].$.value;
+            global.patFamily          = result.Bundle.entry[i].resource[0].Patient[0].name[0].family[0].$.value;
+            global.patGender          = result.Bundle.entry[i].resource[0].Patient[0].gender[0].$.value;
+            global.patDOB1            = result.Bundle.entry[i].resource[0].Patient[0].birthDate[0].$.value;
+            global.patDOB2            = result.Bundle.entry[i].resource[0].Patient[0].birthDate[0].extension[0].valueDateTime[0].$.value;
+          }
+
+          if (tempResource2 == "Organization")  {
+            // Organization resource
+            global.orgODS             = result.Bundle.entry[i].resource[0].Organization[0].identifier[0].value[0].$.value;
+            global.orgName            = result.Bundle.entry[i].resource[0].Organization[0].name[0].$.value;
+          }
+
+          if (tempResource2 == "Practitioner")  {
+            // Practitioner resource
+            global.pracPrefix         = result.Bundle.entry[i].resource[0].Practitioner[0].name[0].prefix[0].$.value;
+            global.pracGiven          = result.Bundle.entry[i].resource[0].Practitioner[0].name[0].given[0].$.value;
+            global.pracFamily         = result.Bundle.entry[i].resource[0].Practitioner[0].name[0].family[0].$.value;
+            global.pracSDS            = result.Bundle.entry[i].resource[0].Practitioner[0].identifier[0].value[0].$.value;
+          }
+
+        };
+
+        console.log("vaccCount :" + global.vaccCount);
+
+    });
+
+  });
+
+    res.render("nrl_multi.ejs");
+    
+});
+
+
+app.get("/single",function(req,res){
+
+  console.log("Attempting to parse: " + NEMSfileToParse);
+
+  fs.readFile('./' + NEMSfileToParse, (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+
+    parser.parseString(data, function (err, result) {
+        //console.dir(result);
+        //console.log(util.inspect(result, false, null));
+
+        var numberOfResources = result.Bundle.entry.length - 1;
+        console.log("NEMS numberOfResources : " + numberOfResources);
+        console.log("NEMS type : " + typeof numberOfResources);
 
         // MessageHeader resource
         global.type = result.Bundle.type[0].$.value;
