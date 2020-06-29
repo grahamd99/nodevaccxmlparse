ada0d9b98a0ffce1f79644509267ce1b54056c9f
grahamd99 <graham.dodsworth@nhs.net>
2020-06-29 11:21:20 +0100

    add basic HTML navidation and rendering for Obs

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var NEMSfileToParse = "vacc_example_nems.xml";
 //var NRLfileToParse = "vacc_example_nrl_device.xml";
 var NRLVaccfileToParse = "vacc_example_nrl_device3.xml";
 var NRLAllergyfileToParse = "allergy_example.xml";
+var NRLObservationfileToParse = "obs_example_nrl.xml";
 
 global.fileToValidate = "test.xml";
 
@@ -20,6 +21,126 @@ app.set("view engine", "ejs");
 
 app.get("/",function(req,res){
   res.render("home");
+});
+
+app.get("/multiobs",function(req,res){
+
+  console.log("Attempting to parse: " + NRLObservationfileToParse);
+
+  fs.readFile('./' + NRLObservationfileToParse, (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+
+    parser.parseString(data, function (err, result) {
+        //console.dir(result);
+        //console.log(util.inspect(result, false, null));
+
+        var numberOfResources = result.Bundle.entry.length - 1;
+        console.log("NRL numberOfResources : " + numberOfResources);
+        console.log("NRL type : " + typeof numberOfResources);
+
+/*
+        global.identifierSystem = [];
+        global.identifierValue = [];
+        global.allergyCodeSNOMED = [];
+        global.allergyCodeDisplay = [];
+        global.listCodeSNOMED  = [];
+        global.listDisplaySNOMED  = [];
+        global.clinicalStatus = [];
+        global.verificationStatus = [];
+        global.type = [];
+        global.asserter = [];
+        global.note = [];
+        global.reactionCodeSNOMED = [];
+        global.reactionDisplaySNOMED = [];
+        global.reactionSeverity = [];
+        global.onsetString = [];
+        global.assertedDate = [];
+*/
+        global.obsCount = 0;
+
+        global.listCodeSNOMED  = result.Bundle.entry[0].resource[0].List[0].code[0].coding[0].code[0].$.value;
+        global.listDisplaySNOMED  = result.Bundle.entry[0].resource[0].List[0].code[0].coding[0].display[0].$.value;
+
+       for (i = 1; i <= numberOfResources; i++) {
+          var tempResource = JSON.stringify( result.Bundle.entry[i].resource[0] );
+          var endBit = tempResource.indexOf('":');
+          var tempResource2 = tempResource.slice(2,endBit);
+          console.log("Resource : " + i + " " + tempResource2);
+/*
+          if (tempResource2 == "Device")  {
+              // its the Device resource so maybe do nothing
+              //console.log("Device meh");
+          }
+
+
+          if (tempResource2 == "AllergyIntolerance")  {
+            //global.vaccCount == global.vaccCount+1;
+            global.allergyCount++;
+            var allergyNumber = i-1;
+
+            // AllergyIntolerance
+            global.identifierSystem[allergyNumber]   = result.Bundle.entry[i].resource[0].AllergyIntolerance[0].identifier[0].system[0].$.value;
+            global.identifierValue[allergyNumber]    = result.Bundle.entry[i].resource[0].AllergyIntolerance[0].identifier[0].value[0].$.value;
+            global.allergyCodeSNOMED[allergyNumber]  = result.Bundle.entry[i].resource[0].AllergyIntolerance[0].code[0].coding[0].code[0].$.value;
+            global.allergyCodeDisplay[allergyNumber] = result.Bundle.entry[i].resource[0].AllergyIntolerance[0].code[0].coding[0].display[0].$.value;
+            global.clinicalStatus[allergyNumber]     = result.Bundle.entry[i].resource[0].AllergyIntolerance[0].clinicalStatus[0].$.value;
+            global.verificationStatus[allergyNumber] = result.Bundle.entry[i].resource[0].AllergyIntolerance[0].verificationStatus[0].$.value;
+            global.type[allergyNumber]               = result.Bundle.entry[i].resource[0].AllergyIntolerance[0].type[0].$.value;
+            global.asserter[allergyNumber]           = result.Bundle.entry[i].resource[0].AllergyIntolerance[0].asserter[0].reference[0].$.value;
+            global.note[allergyNumber]               = result.Bundle.entry[i].resource[0].AllergyIntolerance[0].note[0].text[0].$.value;
+            global.reactionCodeSNOMED[allergyNumber]    = result.Bundle.entry[i].resource[0].AllergyIntolerance[0].reaction[0].manifestation[0].coding[0].code[0].$.value;
+            global.reactionDisplaySNOMED[allergyNumber] = result.Bundle.entry[i].resource[0].AllergyIntolerance[0].reaction[0].manifestation[0].coding[0].display[0].$.value;
+            global.reactionSeverity[allergyNumber]      = result.Bundle.entry[i].resource[0].AllergyIntolerance[0].reaction[0].severity[0].$.value;
+            global.onsetString[allergyNumber]           = result.Bundle.entry[i].resource[0].AllergyIntolerance[0].onsetString[0].$.value;
+            global.assertedDate[allergyNumber]           = result.Bundle.entry[i].resource[0].AllergyIntolerance[0].assertedDate[0].$.value;
+
+          }
+
+          if (tempResource2 == "HealthcareService")  {
+            // HealthcareService resource
+            global.hcsSNOMED           = result.Bundle.entry[i].resource[0].HealthcareService[0].type[0].coding[0].code[0].$.value;
+            global.hcsDisplay          = result.Bundle.entry[i].resource[0].HealthcareService[0].type[0].coding[0].display[0].$.value;
+          }
+
+          if (tempResource2 == "Patient")  {
+            // Patient resource
+            global.nhsnumber          = result.Bundle.entry[i].resource[0].Patient[0].identifier[0].value[0].$.value;
+            global.patGiven           = result.Bundle.entry[i].resource[0].Patient[0].name[0].given[0].$.value;
+            global.patFamily          = result.Bundle.entry[i].resource[0].Patient[0].name[0].family[0].$.value;
+            global.patGender          = result.Bundle.entry[i].resource[0].Patient[0].gender[0].$.value;
+            global.patDOB1            = result.Bundle.entry[i].resource[0].Patient[0].birthDate[0].$.value;
+            global.patDOB2            = result.Bundle.entry[i].resource[0].Patient[0].birthDate[0].extension[0].valueDateTime[0].$.value;
+          }
+
+          if (tempResource2 == "Organization")  {
+            // Organization resource
+            global.orgODS             = result.Bundle.entry[i].resource[0].Organization[0].identifier[0].value[0].$.value;
+            global.orgName            = result.Bundle.entry[i].resource[0].Organization[0].name[0].$.value;
+          }
+
+          if (tempResource2 == "Practitioner")  {
+            // Practitioner resource
+            global.pracId             = result.Bundle.entry[i].resource[0].Practitioner[0].id[0].$.value;
+            global.pracPrefix         = result.Bundle.entry[i].resource[0].Practitioner[0].name[0].prefix[0].$.value;
+            global.pracGiven          = result.Bundle.entry[i].resource[0].Practitioner[0].name[0].given[0].$.value;
+            global.pracFamily         = result.Bundle.entry[i].resource[0].Practitioner[0].name[0].family[0].$.value;
+            global.pracSDS            = result.Bundle.entry[i].resource[0].Practitioner[0].identifier[0].value[0].$.value;
+          }
+*/
+        };
+
+        //console.log("obsCount :" + global.allergyCount);
+        res.render("nrl_multi_obs.ejs");
+
+    });
+
+  });
+
+
+    
 });
 
 
